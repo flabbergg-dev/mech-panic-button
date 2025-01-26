310125168817bc0e524501fa5116159c60babd2b
Greg-466 <gregor.gr20@gmail.com>
2025-01-26 14:49:29 -0400

    removed unused logic

--- components/navigation/bottom.navigation.tsx
+++ components/navigation/bottom.navigation.tsx
@@ -8,8 +8,6 @@ import {
   UserIcon,
 } from "lucide-react"
 import { cn } from "@/lib/utils"
-import Link from "next/link"
-import { usePathname } from 'next/navigation'
 
 interface BottomNavigationProps {
   activeTab: string
@@ -22,14 +20,12 @@ export const BottomNavigation = ({
   onTabChange,
   userRole = "Mechanic"
 }: BottomNavigationProps) => {
-  const pathname = usePathname()
-  console.log(pathname)
+
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 max-w-md mx-auto rounded-t-3xl">
       <div className="flex justify-around items-center h-16 max-w-md mx-auto px-4">
         <NavItem
           id="home"
-          pathname={pathname}
           icon={<HomeIcon />}
           isActive={activeTab === "home"}
           onClick={() => onTabChange("home")}
@@ -38,7 +34,6 @@ export const BottomNavigation = ({
         {userRole === "Customer" && (
           <NavItem
             id="map"
-            pathname={pathname}
             icon={<PinIcon />}
             isActive={activeTab === "map"}
             onClick={() => onTabChange("map")}
@@ -46,21 +41,18 @@ export const BottomNavigation = ({
         )}
         <NavItem
           id="history"
-          pathname={pathname}
           icon={<HistoryIcon />}
           isActive={activeTab === "history"}
           onClick={() => onTabChange("history")}
         />
         <NavItem
           id="profile"
-          pathname={pathname}
           icon={<UserIcon />}
           isActive={activeTab === "profile"}
           onClick={() => onTabChange("profile")}
         />
         <NavItem
           id="settings"
-          pathname={pathname}
           icon={<SettingsIcon />}
           isActive={activeTab === "settings"}
           onClick={() => onTabChange("settings")}
@@ -72,13 +64,12 @@ export const BottomNavigation = ({
 
 interface NavItemProps {
   id: string
-  pathname: string
   icon: React.ReactNode
   isActive: boolean
   onClick: () => void
 }
 
-const NavItem = ({ id, pathname, icon, isActive, onClick }: NavItemProps) => {
+const NavItem = ({ id, icon, isActive, onClick }: NavItemProps) => {
   return (
     <button
       onClick={onClick}
