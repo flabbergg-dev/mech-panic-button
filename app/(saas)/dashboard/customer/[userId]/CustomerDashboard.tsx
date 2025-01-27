f4249dea4fe0890c2e6f855480516a6d91aae0e3
Greg-466 <gregor.gr20@gmail.com>
2025-01-26 23:53:51 -0400

    feat: refactor CustomerDashboard to replace ClientDashboard with new components and improve layout

--- app/(saas)/dashboard/customer/[userId]/CustomerDashboard.tsx
+++ app/(saas)/dashboard/customer/[userId]/CustomerDashboard.tsx
@@ -6,7 +6,9 @@ import { MapDashboard } from "@/components/dashboard/MapDashboard/MapDashboard"
 import { useUser } from "@clerk/nextjs"
 import { useState, Suspense } from "react"
 import { SkeletonBasic } from "@/components/Skeletons/SkeletonBasic"
-import { ClientDashboard } from "@/components/dashboard/ClientDashboard/ClientDashboard"
+import { RippleComp } from "@/components/Animated/RippleComp"
+import { MechPanicButton } from "@/components/Buttons/MechPanicButton"
+import BentoGrid from "@/components/BentoBoxes/BentoGrid"
 
 export default function CustomerDashboard() {
   const { user } = useUser()
@@ -19,7 +21,16 @@ export default function CustomerDashboard() {
   const renderContent = () => {
     switch (activeTab) {
       case "home":
-        return <ClientDashboard />
+        return (
+          <>
+            <div className="flex items-center justify-center min-h-screen">
+              <RippleComp>
+                <MechPanicButton user={user} />
+              </RippleComp>
+            </div>
+            <BentoGrid user={user} />
+          </>
+        )
       case "map":
         return <MapDashboard />
         case "settings":
@@ -32,7 +43,7 @@ export default function CustomerDashboard() {
         return <Profile />
         // TODO: add contact
       default:
-        return <ClientDashboard />
+        return <CustomerDashboard />
     }
   }
 
